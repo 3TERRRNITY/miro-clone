5c99224960722e75723af94c36607a69e726c330
f.nedbalskiy <3terrrnity@gmail.com>
2026-04-29 17:16:36 +0300

    update router

--- src/features/board-list/board-list.page.tsx
+++ src/features/board-list/board-list.page.tsx
@@ -1,7 +1,11 @@
+import { ROUTES } from "@/shared/model/routes";
+import { href, Link } from "react-router-dom";
+
 function AppBoardList() {
   return (
     <div>
       AppBoardList
+      <Link to={href(ROUTES.BOARD, { boardId: "1" })}>Board 1 </Link>
     </div>
   );
 }
--- src/features/board/board.page.tsx
+++ src/features/board/board.page.tsx
@@ -1,6 +1,9 @@
+import type { PathParams, ROUTES } from "@/shared/model/routes";
+import { useParams } from "react-router-dom";
 
 function AppBoard() {
-  return <div>AppBoard</div>;
+  const params = useParams<PathParams[typeof ROUTES.BOARD]>()
+  return <div>AppBoard {params.boardId}</div>;
 }
 
 export const Component = AppBoard;
