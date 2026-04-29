fc08078a2d97eaa3f659a00f4f7c52c667b1838f
f.nedbalskiy <3terrrnity@gmail.com>
2026-04-29 17:27:39 +0300

    baseurl fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,15 @@
 import { AppHeader } from "@/features/header";
-import { Outlet } from "react-router-dom";
+import { ROUTES } from "@/shared/model/routes";
+import { Outlet, useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+
+  const isAuth =
+    location.pathname === ROUTES.LOGIN || location.pathname === ROUTES.REGISTER;
   return (
     <div>
-      <AppHeader />
+      {!isAuth && <AppHeader />}
       <Outlet />
     </div>
   );
